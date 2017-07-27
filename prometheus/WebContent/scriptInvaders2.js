85aeb5f788f0ece2e36f996394835f5e7e857224
UTILISATEUR <UTILISATEUR@192.168.1.169>
2017-07-27 10:11:00 +0200

    squelette

---
+++ prometheus/WebContent/scriptInvaders2.js
@@ -0,0 +1,17 @@
+function tire(){
+	
+	//le missile apparait devant le vaisseau :initialisation
+	
+	if(keycode == 38){
+		var missile = document.getElementById("missile");
+		
+	}
+	
+	
+	
+	//le missile monte 
+	
+	//si le missile touche il disparait
+	
+	
+}
