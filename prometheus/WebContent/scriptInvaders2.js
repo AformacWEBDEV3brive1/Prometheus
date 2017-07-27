9e7c56f1a55b84672b4c6903154ca326b0bd4e9c
UTILISATEUR <UTILISATEUR@192.168.1.169>
2017-07-27 12:00:59 +0200

    le missile avance 1 fois

--- prometheus/WebContent/scriptInvaders2.js
+++ prometheus/WebContent/scriptInvaders2.js
@@ -1,17 +1,42 @@
 function tire(){
 	
-	//le missile apparait devant le vaisseau :initialisation
 	
-	if(keycode == 38){
-		var missile = document.getElementById("missile");
+	var ecran=0;
+	
+	//le missile apparait devant le vaisseau :initialisation
+	var positionY=document.getElementById("vaisseau").style.marginTop;
+	var positionX=document.getElementById("vaisseau").style.marginLeft;
+	
+	var missile = document.getElementById("missile");
+	
+	missile.style.marginTop =positionY;
+	missile.style.marginLeft=positionX;
+	
+	//le missile est charger et tirer
+	
+	if(event.keyCode == 38){
 		
+		missile.style.display = "block";
 	}
 	
 	
 	
 	//le missile monte 
 	
-	//si le missile touche il disparait
+	if(missile.style.display = "block"){
+		var tir=parseInt(missile.style.marginTop);
+		console.log(tir);
+		var limite=1900 ;
+		do{
+			tir=tir-1;
+			missile.style.marginTop=tir+"px";
+			document.getElementById("missile").className="missile shoot";
+		}while(tir<ecran);
+		
+		//document.getElementById("missile").className="missile";
+	}
+	
+	//si le missile touche ou sort de l'ecran il disparait
 	
 	
 }
