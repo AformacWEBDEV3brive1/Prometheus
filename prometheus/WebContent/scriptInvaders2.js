7d55b362132b60bd0a35f821501ac58868821aba
UTILISATEUR <UTILISATEUR@192.168.1.166>
2017-07-28 16:07:56 +0200

    restant :
    	-identifié les aliens
    	-gestion du tir en mouvement
    
    mega reboot de matthieu

--- prometheus/WebContent/scriptInvaders2.js
+++ prometheus/WebContent/scriptInvaders2.js
@@ -1,51 +1,78 @@
-function tire() {
+function missile() {
 
-// le missile apparait devant le vaisseau :initialisation
-    var positionY = document.getElementById("vaisseau").style.marginTop;
-    var positionX = document.getElementById("vaisseau").style.marginLeft;
-    var missile = document.getElementById("missile");
-    missile.style.marginTop = positionY;
-    missile.style.marginLeft = positionX;
-    // le missile est charger et tirer
-    if (event.keyCode == 90) {
+        
+        
+        var vie = document.getElementById("vie").clientHeight;
+        var go = document.getElementById("go").clientHeight;
+        var missile = document.getElementById("missile");
 
-        missile.style.display = "block";
-    }
+        if (missile == null) {
+            console.log("l'element nexiste pas");
+            projectile = document.createElement("div");
+            projectile.id = "missile";
+            projectile.style.width = "10px";
+            projectile.style.height = "10px";
+            projectile.style.backgroundColor = "black";
+            projectile.style.bottom = "70px";
+            projectile.style.position = "absolute";
+            var vaisseau = document.getElementById("vaisseau");
 
-// le missile monte
-    if (missile.style.display = "block") {
-        var tir = parseInt(missile.style.marginTop);
-        console.log(tir);
-        intervalID = setInterval(function () {
-
-            do {
-
-                tir = tir - 25;
-                missile.style.marginTop = tir + "px";
-                console.log(tir);
-            } while (tir <= 0);
-        }, 100);
-        clearInterval(intervalID);
-// si le missile touche ou sort de l'ecran il disparait
-        if (tir < 0) {
-            clearInterval(intervalID);
-            // missile.style.display = "none";
+            document.body.insertBefore(projectile, vaisseau);
         }
 
-    }
+        missile = document.getElementById("missile");
+
+        var html=950;
+        var body=document.getElementsByTagName("body").clientHeight;
+        
+      
+        
+        
+        console.log(" html : "+html+" body : "+body);
+        
+        var plateau = parseInt(document.getElementById("plateau").clientHeight);
+          console.log("vie : "+vie+" go : "+go+" plateau : "+plateau);
+        console.log("l'element existe");
+        var distance = parseInt(document.getElementById("missile").style.bottom);
+        
+        
+        var cible = vie+go+plateau;
+        var process = setInterval(function () {
+            distance = distance + 1;
+            missile.style.bottom = distance + "px";
+
+
+           // console.log("distance : "+distance+" plateau : "+plateau);
+            if (distance >(html-cible)) {
+                clearInterval(process);
+                suppr(missile);
+                removeElement(plateau);
+
+            }
+
+
+        }, 1);
+          // Suppression de la div.
+    
+
 
 }
-
 //si le missile touche ou sort de l'ecran il disparait
 
+function suppr(missile) {
+	missile.parentNode.removeChild(missile);
+}
+function removeElement(et) {
+	var plateau = document.getElementById("plateau");
+	plateau.parentNode.removeChild(plateau);
+}
 
 
 
 //Déplacer le vaisseau sur la droite et la gauche
-
 document.addEventListener('keydown', direction, false);
 function direction(event) {
-
+r
     if (event.keyCode == 81) {
         var vaisseau = parseInt(document.getElementById("vaisseau").style.marginLeft);
         vaisseau = vaisseau - 50;
