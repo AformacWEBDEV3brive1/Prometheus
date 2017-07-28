9d9cad1c7539d11294793b50bbf58bc63cce9602
UTILISATEUR <UTILISATEUR@192.168.1.166>
2017-07-28 09:13:37 +0200

    commit Thomas : git ok ajout js > deplacement droite gauche du vaisseau + photo vaisseau

--- prometheus/WebContent/scriptInvaders2.js
+++ prometheus/WebContent/scriptInvaders2.js
@@ -1,44 +1,76 @@
 function tire() {
 
-	// le missile apparait devant le vaisseau :initialisation
-	var positionY = document.getElementById("vaisseau").style.marginTop;
-	var positionX = document.getElementById("vaisseau").style.marginLeft;
+// le missile apparait devant le vaisseau :initialisation
+    var positionY = document.getElementById("vaisseau").style.marginTop;
+    var positionX = document.getElementById("vaisseau").style.marginLeft;
+    var missile = document.getElementById("missile");
+    missile.style.marginTop = positionY;
+    missile.style.marginLeft = positionX;
+    // le missile est charger et tirer
+    if (event.keyCode == 90) {
 
-	var missile = document.getElementById("missile");
+        missile.style.display = "block";
+    }
 
-	missile.style.marginTop = positionY;
-	missile.style.marginLeft = positionX;
+// le missile monte
+    if (missile.style.display = "block") {
+        var tir = parseInt(missile.style.marginTop);
+        console.log(tir);
+        intervalID = setInterval(function () {
 
-	// le missile est charger et tirer
-	if (event.keyCode == 38) {
+            do {
 
-		missile.style.display = "block";
-	}
+                tir = tir - 25;
+                missile.style.marginTop = tir + "px";
+                console.log(tir);
+            } while (tir <= 0);
+        }, 100);
+        clearInterval(intervalID);
+// si le missile touche ou sort de l'ecran il disparait
+        if (tir < 0) {
+            clearInterval(intervalID);
+            // missile.style.display = "none";
+        }
 
-	// le missile monte
-	if (missile.style.display = "block") {
-		var tir = parseInt(missile.style.marginTop);
-		console.log(tir);
-
-		intervalID = setInterval(function() {
-
-			do {
-
-				tir = tir - 25;
-				missile.style.marginTop = tir + "px";
-				console.log(tir);
-
-			} while (tir <= 0);
-		
-		}, 100);
-
-		
-	}
-	clearInterval(intervalID);
-	// si le missile touche ou sort de l'ecran il disparait
-	if (tir < 0) {
-		clearInterval(intervalID);
-		// missile.style.display = "none";
-	}
+    }
 
 }
+
+//si le missile touche ou sort de l'ecran il disparait
+
+
+
+
+//Déplacer le vaisseau sur la droite et la gauche
+
+document.addEventListener('keydown', direction, false);
+function direction(event) {
+
+    if (event.keyCode == 81) {
+        var vaisseau = parseInt(document.getElementById("vaisseau").style.marginLeft);
+        vaisseau = vaisseau - 50;
+        document.getElementById("vaisseau").style.marginLeft = vaisseau + "px";
+    }
+    var x = parseInt(document.getElementById("vaisseau").style.marginLeft);
+    if (x <= -5) {
+        x = -5;
+        document.getElementById("vaisseau").style.marginLeft = x + "px";
+    }
+
+    if (event.keyCode == 68) {
+        var vaisseau = parseInt(document.getElementById("vaisseau").style.marginLeft);
+        vaisseau = vaisseau + 50;
+        document.getElementById("vaisseau").style.marginLeft = vaisseau + "px";
+    }
+
+    var x = parseInt(document.getElementById("vaisseau").style.marginLeft);
+    if (x >= 1840) {
+        x = 1840;
+        document.getElementById("vaisseau").style.marginLeft = x + "px";
+    }
+}
+
+// fin déplacement
+
+
+
