24eb6b894b2ee8379e4715e6e87cf8a23f69c289
UTILISATEUR <UTILISATEUR@192.168.1.169>
2017-07-28 08:09:56 +0200

    test pour voir

--- prometheus/WebContent/scriptInvaders2.js
+++ prometheus/WebContent/scriptInvaders2.js
@@ -1,42 +1,44 @@
-function tire(){
-	
-	
-	var ecran=0;
-	
-	//le missile apparait devant le vaisseau :initialisation
-	var positionY=document.getElementById("vaisseau").style.marginTop;
-	var positionX=document.getElementById("vaisseau").style.marginLeft;
-	
+function tire() {
+
+	// le missile apparait devant le vaisseau :initialisation
+	var positionY = document.getElementById("vaisseau").style.marginTop;
+	var positionX = document.getElementById("vaisseau").style.marginLeft;
+
 	var missile = document.getElementById("missile");
-	
-	missile.style.marginTop =positionY;
-	missile.style.marginLeft=positionX;
-	
-	//le missile est charger et tirer
-	
-	if(event.keyCode == 38){
-		
+
+	missile.style.marginTop = positionY;
+	missile.style.marginLeft = positionX;
+
+	// le missile est charger et tirer
+	if (event.keyCode == 38) {
+
 		missile.style.display = "block";
 	}
-	
-	
-	
-	//le missile monte 
-	
-	if(missile.style.display = "block"){
-		var tir=parseInt(missile.style.marginTop);
+
+	// le missile monte
+	if (missile.style.display = "block") {
+		var tir = parseInt(missile.style.marginTop);
 		console.log(tir);
-		var limite=1900 ;
-		do{
-			tir=tir-1;
-			missile.style.marginTop=tir+"px";
-			document.getElementById("missile").className="missile shoot";
-		}while(tir<ecran);
+
+		intervalID = setInterval(function() {
+
+			do {
+
+				tir = tir - 25;
+				missile.style.marginTop = tir + "px";
+				console.log(tir);
+
+			} while (tir <= 0);
 		
-		//document.getElementById("missile").className="missile";
+		}, 100);
+
+		
 	}
-	
-	//si le missile touche ou sort de l'ecran il disparait
-	
-	
+	clearInterval(intervalID);
+	// si le missile touche ou sort de l'ecran il disparait
+	if (tir < 0) {
+		clearInterval(intervalID);
+		// missile.style.display = "none";
+	}
+
 }
